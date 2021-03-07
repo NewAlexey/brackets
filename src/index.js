7e91e28b043d15e05c4d33e681d499bb83d52748
NewAlexey <alexey.krupenia@gmail.com>
2021-03-07 12:35:12 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let openBrackets = [];
+    let closeBrackets = [];
+    let supportArray = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        openBrackets.push(bracketsConfig[i][0]);
+        closeBrackets.push(bracketsConfig[i][1]);
+    }
+    for (let i = 0; i < str.length; i++) {
+        let openIndex = openBrackets.indexOf(str[i]);
+        let closeIndex = closeBrackets.indexOf(str[i]);
+        if (openIndex > -1) {
+            openIndex === closeIndex
+                ? supportArray.length > 0 &&
+                  supportArray[supportArray.length - 1] === closeIndex
+                    ? supportArray.pop()
+                    : supportArray.push(openIndex)
+                : supportArray.push(openIndex);
+        } else {
+            if (supportArray.length > 0) {
+                if (closeIndex !== supportArray.pop()) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+    return supportArray.length === 0 ? true : false;
+};
